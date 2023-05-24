2fedda1f2d83be40b7400159a72aac08a559b9e4
chidimyke <ejimonuchidiebere@gmail.com>
2023-05-24 01:51:09 +0100

    sample fetch request

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Style guide
+// import fetch from "node-fetch";
+function calculate() {
+  fetch("items.json")
+    // .then((feedback) => console.log(feedback))
+    .then((feedback) => feedback.json())
+    .then((data) => (document.body.innerHTML = data[0].text));
+}
+
+calculate();
